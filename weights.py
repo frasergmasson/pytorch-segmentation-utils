14b67aea814c2a2122040bd03a6c7956ed538e19
nofareyoker <nofareyoker@gmail.com>
2023-04-24 12:14:52 +0100

    weights can change device

--- weights.py
+++ weights.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-def calculate_class_weights(train_set,n_labels):
+def calculate_class_weights(train_set,n_labels,device="cpu"):
   #Determine no. pixels of each class in training set
   n_pixels = np.zeros(n_labels)
   for _,mask in train_set:
@@ -11,4 +11,4 @@ def calculate_class_weights(train_set,n_labels):
 
   total_pixels = np.sum(n_pixels)
   weights = total_pixels / n_pixels
-  return torch.Tensor(weights / np.sum(weights))
+  return torch.Tensor(weights / np.sum(weights)).to(device)

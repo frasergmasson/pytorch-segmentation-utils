cb92d2bf8835a7cd3ef08b6735e95581e6c66366
nofareyoker <nofareyoker@gmail.com>
2023-04-26 15:05:23 +0100

    more basic metrics

--- evaluation.py
+++ evaluation.py
@@ -16,6 +16,15 @@ def pixel_accuracy_for_class(prediction,truth,label):
     return None
   return (correct/total).item()
 
+def true_positive_all_positive_classwise(prediction,truth,label):
+  label_indices = truth == label
+  correct = (prediction[label_indices] == truth[label_indices]).sum()
+  total = label_indices.sum()
+  if total == 0:
+    return None
+  return correct,total
+
+
 def pixel_accuracy_all_classes(prediction,truth,n_labels):
   return [pixel_accuracy_for_class(prediction,truth,label) for label in range(n_labels)]
 
@@ -26,6 +35,14 @@ def iou_for_class(prediction,truth,label):
   if union == 0:
     return None
   return (intersect/union).item()
+
+def intersect_union_classwise(prediction,truth,label):
+  class_indices = truth == label
+  intersect = (prediction[class_indices] == label).sum()
+  union = class_indices.sum() + (prediction == label).sum() - intersect
+  if union == 0:
+    return None
+  return intersect,union
 
 def iou_all_classes(prediction,truth,n_labels):
   return [iou_for_class(prediction,truth,label) for label in range(n_labels)]
@@ -41,6 +58,17 @@ def f1(prediction,truth,label):
     return None
   return (2*tp)/denominator
 
+def tp_fp_fn_classwise(prediction,truth,label):
+  positive_indices = truth == label
+  negative_indices = truth != label
+  tp = (truth[positive_indices] == prediction[positive_indices]).sum().item()
+  fp = (truth[negative_indices] != prediction[negative_indices]).sum().item()
+  fn = (truth[positive_indices] != prediction[positive_indices]).sum().item()
+  denominator = 2*tp + fp + fn
+  if denominator == 0:
+    return None
+  return tp,fp,fn
+
 def f1_all_classes(prediction,truth,n_labels):
   return [f1(prediction,truth,label) for label in range(n_labels)]
 
@@ -53,11 +81,14 @@ class MetricManager():
     self.function_keywords = {}
     self.stats = {}
     self.labels = labels
+    self.n = {}
 
-  def add_metric(self,name,function,**keywords):
+  def add_metric(self,name,function,record_n=False,**keywords):
     self.functions[name] = function
     self.function_keywords[name] = keywords
     self.stats[name] = []
+    if record_n:
+      self.n[name] = []
 
   def apply_metrics(self,prediction,truth):
     label_pred = torch.argmax(prediction,1)
@@ -65,7 +96,11 @@ class MetricManager():
     for name in self.functions.keys():
       result = self.functions[name](
         label_pred,label_truth,**self.function_keywords[name])
-      self.stats[name].append(result)
+      if name in self.n:
+        self.stats[name].append(result[0])
+        self.n[name].append(result[1])
+      else:
+        self.stats[name].append(result)
 
   def get_metric(self,name):
     return np.array(self.stats[name])
